f59d2a5d496cb2910178f5e66c0f69e96611c13a
PuP <allexander.bergmans@outlook.com>
2025-01-24 14:19:52 +0100

    Add project files.

---
+++ bunasa_mandarin/bunasa_mandarin.cpp
@@ -0,0 +1,16 @@
+﻿#include "bunasa_mandarin.h"
+
+using namespace std;
+
+
+double VERSION = 0.1;
+string CODENAME = "Manderin";
+
+int main()
+{
+	cout << "Bunasa: VERSION:" " " << VERSION << " " "CODENAME:" " " << CODENAME << endl;
+	cout << "Author: Allexander Bergmans." << endl;
+	cout << "Co-Author: Toon Schuermans." << endl;
+
+	return 0;
+}
