c6bea3aa3b9309216cd27f1fd832c219d24267ca
bmallexander <allexanderlinux@gmail.com>
2025-01-25 18:00:32 +0100

    Update IMGUI

--- bunasa_mandarin/bunasa_mandarin.cpp
+++ bunasa_mandarin/bunasa_mandarin.cpp
@@ -1,13 +1,85 @@
 ﻿#include <iostream>
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+#include <GLFW/glfw3.h>
+#include <GL/gl.h>
+
 #include "Headers/info.h"
+
+void SetupImGui(GLFWwindow* window)
+{
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    (void)io;
+    ImGui::StyleColorsDark();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 130");
+}
+
+void RenderImGui()
+{
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+
+    ImGui::Begin("Hello, ImGui!");
+    ImGui::Text("This is a simple example!");
+    ImGui::End();
+
+    ImGui::Render();
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+}
 
 using namespace std;
 
 int main()
 {
+
     cout << "Bunasa: VERSION: " << VERSION << " CODENAME: " << CODENAME << endl;
     cout << "Author: Allexander Bergmans." << endl;
     cout << "Co-Author: Toon Schuermans." << endl;
+
+    // Initialize GLFW
+    if (!glfwInit())
+        return -1;
+
+    // Create a windowed mode window and its OpenGL context
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Bunasa", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+
+    // Setup ImGui
+    SetupImGui(window);
+
+    // Main loop
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+
+        // Clear screen
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Render ImGui
+        RenderImGui();
+
+        // Swap buffers
+        glfwSwapBuffers(window);
+    }
+
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+    glfwDestroyWindow(window);
+    glfwTerminate();
 
     return 0;
 }
